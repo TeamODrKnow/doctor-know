d39653d3d433b8fb8beedb3912a442d0ed47f99f
J. Matthew Landis <jml168@pitt.edu>
2015-03-03 12:46:48 -0500

    initial commit
    Complete copy of original working site

---
+++ static/scripts/appsPanel.js
@@ -0,0 +1,6 @@
+// Script to handle app buttons on right side of wordcloud
+
+
+$('#myStateButton').on('click', function () {
+    $(this).button('complete') // button text will be "finished!"
+  })
