f3fa52c11b343c3076fa67d7a7fad4dcef60fb29
Richard Miller <rjm2692@gmail.com>
2015-04-07 12:28:20 -0400

    forgot to push javascript file on last commit

--- static/scripts/words.js
+++ static/scripts/words.js
@@ -1,6 +1,31 @@
 // this function adds a new email input to our form
+var wordcounter = 0;
 function addWord()
 {
 	wordcounter++;
-	document.getElementById('wordcontainer').innerHTML+='<div>Word '+wordcounter+': <input type="text" name="word"><br></div>';
+	if (wordcounter == 1)
+	{
+		document.getElementById('wordcontainer').innerHTML+='<button style="color: black !important;" type="button" id="rbutton" onclick="removeWord()">Remove Word</button><p>Word '+wordcounter+': <input style="color: black !important;" type="text" name="word"><br></p>';
+	}
+	else
+	{
+		document.getElementById('wordcontainer').innerHTML+='<p>Word '+wordcounter+': <input style="color: black !important;" type="text" name="word"><br></p>';
+	}
 }
+function removeWord()
+{
+	wordcounter--;
+	listNodes = document.getElementsByTagName("p");
+	parent = document.getElementById('wordcontainer');
+	if (wordcounter == 0)
+	{
+		parent.removeChild(listNodes[wordcounter]);
+		child = document.getElementById("rbutton");
+		parent.removeChild(child);
+	}
+	else
+	{
+		parent.removeChild(listNodes[wordcounter])
+	}
+}
+	
